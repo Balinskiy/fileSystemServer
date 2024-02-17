4cbd67288c4aa55ca94d2bab7a58e3a36ce22056
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2024-02-17 15:35:34 +0200

    add middleware about extension

---
+++ middleWares/checkExtension.js
@@ -0,0 +1,15 @@
+import { HttpError } from "../helpers/HttpError";
+
+export const checkExtension = (req, res, next) => {
+  const EXTENSIONS = ["txt", "pdf", "mp4", "doc", "js"];
+
+  const fileName = req.body.filename;
+  const check = fileName.split(".");
+  const extension = check[check.length - 1];
+  const message = `Sorry, the application does not support files with ${extension} extension`;
+  const isValidExtension = EXTENSIONS.some((item) => item === extension);
+  if (!isValidExtension) {
+    next(HttpError(400, message));
+  }
+  next();
+};
--- routes/filesRouter.js
+++ routes/filesRouter.js
@@ -1,8 +1,16 @@
 import express from "express";
 import { createFile } from "../controllers/filesController";
+import { validateBody } from "../helpers/validateBody";
+import { createFileSchema } from "../schemas/filesSchemas";
+import { checkExtension } from "../middleWares/checkExtension";
 
 const filesRouter = express.Router();
 
-filesRouter.post("/", createFile);
+filesRouter.post(
+  "/",
+  validateBody(createFileSchema),
+  checkExtension,
+  createFile
+);
 
 export default filesRouter;
